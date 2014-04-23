ad93f932a4423d25684d3edd0358978f4f7565c7
Brandon T <JustBrandonT@gmail.com>
2014-04-23 15:22:02 -0400

    Storyboard Update
    Storyboard layout is complete

---
+++ MinPairs/MLViewController.m
@@ -0,0 +1,29 @@
+//
+//  MLViewController.m
+//  MinPairs
+//
+//  Created by ituser on 4/23/2014.
+//  Copyright (c) 2014 MobiLearning. All rights reserved.
+//
+
+#import "MLViewController.h"
+
+@interface MLViewController ()
+
+@end
+
+@implementation MLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
