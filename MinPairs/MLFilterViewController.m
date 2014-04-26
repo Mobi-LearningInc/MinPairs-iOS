76c9fe9f2f11c9e85f5aa94c56a43144eeadda2d
Brandon <JustBrandonT@gmail.com>
2014-04-26 09:50:35 -0400

    Quick Table Filter

--- MinPairs/MLFilterViewController.m
+++ MinPairs/MLFilterViewController.m
@@ -8,9 +8,10 @@
 
 #import "MLFilterViewController.h"
 
-@interface MLFilterViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
-@property (nonatomic, weak) NSMutableArray* leftSide;
-@property (nonatomic, assign) bool chosen;
+@interface MLFilterViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *leftView;
+@property (weak, nonatomic) IBOutlet UITableView *rightView;
+@property (nonatomic, strong) NSArray* categories;
 @end
 
 @implementation MLFilterViewController
@@ -28,75 +29,44 @@
 {
     [super viewDidLoad];
     
-    self.chosen = false;
-    self.leftSide = [MLSynchronousFilter getCategoriesLeft];
+    MLMainDataProvider* provider = [[MLMainDataProvider alloc] initMainProvider];
     
-    //self.view.transform = CGAffineTransformMakeScale(1, 2);
+    _categories = [provider getCategories];
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     
+    if(!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: @"cell"];
+    }
+    
+    if (tableView == [self leftView])
+    {
+        cell.textLabel.text = [[[self categories] objectAtIndex: indexPath.row] categoryDescription];
+    }
+    return cell;
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == [self leftView])
+    {
+        return [[self categories] count];
+    }
+    return 0;
+}
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
-    return 2;
-}
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
-    return [[self leftSide] count];
-}
-
--(NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    switch (component)
-    {
-        case 0:
-            return [[[self leftSide] objectAtIndex: row] categoryDescription];
-            
-        case 1:
-            if ([self chosen])
-            {
-                NSMutableArray* cat = [MLSynchronousFilter getCorrespondingCategories: [[self leftSide] objectAtIndex: row]];
-                self.chosen = false;
-                return [[cat objectAtIndex: 0] categoryDescription];
-            }
-    }
-    return nil;
-}
-
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
-{
-    switch(component)
-    {
-        case 0: self.chosen = true; [pickerView reloadComponent: 1];
-    }
-}
-
- - (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component
- {
-     return self.view.frame.size.width / 2;
- }
- 
- - (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
- {
-     return 50;
- }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
